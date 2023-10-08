7e49ee11ff0f88009c1c5960e31c5b61d10b729b
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2023-10-08 14:53:26 -0400

    Time: 1226 ms (5.01%), Space: 30.3 MB (85.24%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,29 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        tempStack = [] 
+        result = [0] * len(temperatures)
+
+        for idx, temp in enumerate(temperatures):
+            while tempStack and temp > temperatures[tempStack[-1]]:
+                print(temperatures[tempStack[-1]])
+                # keeping the track of prev idx 
+                prev_idx = tempStack.pop()
+                result[prev_idx] = idx - prev_idx 
+            tempStack.append(idx)
+        return result 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
